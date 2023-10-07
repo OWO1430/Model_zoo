cfc6da5ea688d9a75410efea8028f44d3919c0cf
brian033 <85883730+brian033@users.noreply.github.com>
2023-10-07 16:09:42 +0800

    start devving frontend

--- model_zoo_web/components/navbar.tsx
+++ model_zoo_web/components/navbar.tsx
@@ -4,10 +4,6 @@ import SearchBar from "./searchBar";
 export default function Navbar() {
     return (
         <div className="flex justify-between mt-6 mx-5">
-            <ul className="">
-                <li>TBA</li>
-            </ul>
-            <SearchBar />
             <LoginBTN />
         </div>
     );
