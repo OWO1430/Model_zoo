e1247df963cc1f1ea0f9c488d01cccf4a468e53f
Trinky Su <trinky.cw.su@gmail.com>
2023-10-07 23:56:42 +0800

    model database page

--- model_zoo_web/pages/database/index.tsx
+++ model_zoo_web/pages/database/index.tsx
@@ -1,3 +1,24 @@
-export default function database() {
-    return <h1>哈哈db屁眼</h1>;
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar";
+import SearchBox from "@/components/searchBox";
+import SearchBar from "@/components/searchBar";
+import ModelBlock from "@/components/modelBlock";
+
+// const inter = Inter({ subsets: ["latin"] });
+// function SearchBoxes() {
+//     return (
+//         <div className="bg-white flex flex-col">
+//             <SearchBar />
+//             <ModelBlock />
+//         </div>
+//     );
+// }
+
+export default function Home() {
+    return (
+        <div className="flex ml-24 bg-white">
+            <ModelBlock />
+        </div>
+    );
 }
