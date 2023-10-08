b893b690f77f3af540e6f388ce8a882b01954870
Trinky Su <trinky.cw.su@gmail.com>
2023-10-08 20:48:08 +0800

    Fix: dummyData

--- model_zoo_web/dummyData/model.ts
+++ model_zoo_web/dummyData/model.ts
@@ -1,30 +1,5 @@
-
 export const main = [
 
-    {
-        dataType:"NASA image Data" ,
-        description: "Micro-OCT data from Open Science for Life in Space, images of murine retina.",
-        name: "murine_retina_OSD-557",
-        link: "https://osdr.nasa.gov/bio/repo/data/studies/OSD-557",
-    },
-    {
-        dataType:"NASA image Data" ,
-        description: "Biofilm images in different growing conditions.",
-        name: "biofilm_OSD-628",
-        link: "https://osdr.nasa.gov/bio/repo/data/studies/OSD-628",
-    },
-    {
-        dataType:"NASA RNA_seq" ,
-        description: "RNA sequence data of Arabidopsis thaliana in studying “ epigenetic Changes in Methylation Mutants”.",
-        name: "Arabidopsis thaliana RNA_OSD-427",
-        link: "https://osdr.nasa.gov/bio/repo/data/studies/OSD-427",
-    },
-    {
-        dataType:"NASA RNA_seq" ,
-        description: "RNA sequence data of Arabidopsis thaliana in studying “Plant Gravity Perception\”",
-        name: "Arabidopsis thaliana RNA_OSD-480",
-        link: "https://osdr.nasa.gov/bio/repo/data/studies/OSD-480",
-    },
     {
         "dataType": "Image models",
         "description": "A deep convolutional neural network with 18 layers, commonly used for image classification tasks",
@@ -60,65 +35,5 @@ export const main = [
         "description": "A deep learning model to predict DNA methylation states from sequence data.",
         "name": "EpiDeep",
         "link": "https://epideep.github.io/"
-    },
-    {
-        "dataType": "Earth dataset of Microbiology",
-        "description": "A comprehensive resource that has sequences of microbial genomes found in the human body.",
-        "name": "Human Microbiome Project (HMP) Data",
-        "link": "https://www.hmpdacc.org/hmp/"
-    },
-    {
-        "dataType": "Earth dataset of Microbiology",
-        "description": "The Integrated Microbial Genomes & Microbiomes system offers tools for the analysis of microbial community genomes.",
-        "name": "IMG/M",
-        "link": "https://img.jgi.doe.gov/cgi-bin/m/main.cgi"
-    },
-    {
-        "dataType": "Earth dataset of Cell and Molecular Biology",
-        "description": "Detailed genomic information for over 30 types of cancer.",
-        "name": "The Cancer Genome Atlas (TCGA)",
-        "link": "https://portal.gdc.cancer.gov/"
-    },
-    {
-        "dataType": "Earth dataset of Cell and Molecular Biology",
-        "description": "A public functional genomics data repository supporting MIAME-compliant data submissions.",
-        "name": "Gene Expression Omnibus (GEO)",
-        "link": "https://www.ncbi.nlm.nih.gov/geo/"
-    },
-    {
-        "dataType": "Earth dataset of Plant Biology",
-        "description": "Sequencing of over 1000 different strains of the model plant Arabidopsis.",
-        "name": "The 1001 Genomes Project for Arabidopsis thaliana",
-        "link": "https://1001genomes.org/"
-    },
-    {
-        "dataType": "Earth dataset of Animal Biology",
-        "description": "A comprehensive database on the genetics and genomics of the laboratory mouse.",
-        "name": "Mouse Genome Informatics (MGI)",
-        "link": "http://www.informatics.jax.org/"
-    },
-    {
-        "dataType": "Earth dataset of Animal Biology",
-        "description": "Provides integrated access to curated zebrafish genetic and genomic data.",
-        "name": "Zebrafish Model Organism Database (ZFIN)",
-        "link": "https://zfin.org/"
-    },
-    {
-        "dataType": "Earth dataset of Developmental, Reproductive and Evolutionary Biology",
-        "description": "Datasets aimed at studying craniofacial development and disorders.",
-        "name": "FaceBase",
-        "link": "https://www.facebase.org/"
-    },
-    {
-        "dataType": "Earth dataset of Developmental, Reproductive and Evolutionary Biology",
-        "description": "A repository of phylogenetic information, specifically user-submitted phylogenetic trees and the data used to generate them.",
-        "name": "TreeBASE",
-        "link": "https://www.treebase.org/"
-    },
-    {
-        "dataType": "Earth dataset of Developmental, Reproductive and Evolutionary Biology",
-        "description": "A database to retrieve and compare gene expression patterns in multiple animal species, produced from multiple data types such as RNA-seq, microarrays, and in situ hybridization.",
-        "name": "Bgee",
-        "link": "https://bgee.org/"
     }
 ]
